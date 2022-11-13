c5da36e097ec5a34d27b9724499e30aa0961deb1
SwaroopLightning <sathyaswaroopvandavasi@gmail.com>
2022-11-13 17:27:08 +0530

    added store cart module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/create-user.dto';
-import { User, UserDocument } from './schema/user.schema';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
